7de75ec8f1e850dd2964031102b8c0be936c06dc
Izundu Dan-Ekeh <dizundu@yahoo.com>
2025-02-18 08:40:51 +0100

    feat: add gifting feature for special occasions

--- src/mainRouter.js
+++ src/mainRouter.js
@@ -1,6 +1,7 @@
 const userRouter = require('./routes/userRoutes');
 const reportRouter = require('./routes/reportRoute');
 const loveRequestRouter = require('./routes/loveRequestRoutes');
+const giftRouter = require('./routes/giftRoutes');
 const express = require('express');
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 router.use('/users', userRouter);
 router.use('/reports', reportRouter);
 router.use('/love-requests', loveRequestRouter);
+router.use('/gifts', giftRouter);
 
 module.exports = router;
